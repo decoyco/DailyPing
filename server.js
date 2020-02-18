5f48ceaa7cabccb4f59a7f63473c12988c26164a
Josh Coyco <jac6308@gmail.com>
2020-02-18 13:47:42 -0800

    fix

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ var transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
       user: process.env.EMAIL,
-      pass: process.env.Email_PASSWORD
+      pass: process.env.EMAIL_PASSWORD
     }
   });
 const weather = require('openweather-apis')
